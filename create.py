ca620973b47be6437860bbc31e663247e260d77c
Bujbujbuj <49245903+Bujbujbuj@users.noreply.github.com>
2020-04-18 16:25:42 -0400

    Update create.py

--- create.py
+++ create.py
@@ -2,13 +2,18 @@
 # create.py contains functions to blend pictures appropriately.
 #
 #
+import random
 
 from pic import *
 
 
-def blender(first, master, count):
-    _w = 1024
-    _h = 768
+# blender:
+#   first = the picture to be added
+#   master = the aggregated photo
+#   count = tracker used for the weight of the photo, keep balance
+#   _w & _h = width/height, allow user input.
+# FUTURE: make user options for width & height
+def blender(first, master, count, _w=1024, _h=768):
     a = 1 / count
 
     one = Image.fromarray(load_pic(first, _w, _h))
@@ -18,5 +23,30 @@ def blender(first, master, count):
         two = Image.fromarray(load_pic(first, _w, _h))
 
     three = Image.blend(two, one, a)
-
     return three
+
+
+# rand_seed:
+# create a picture based on a random seed.
+#   var = the "random" seed
+#   _w = width, set to "typical" 1024
+#   _h = height, set to "typical" 768
+#   intensity = how dense the picture is. Pass in 10 to see effect.
+def rand_seed(var, _w=1024, _h=768, intensity=1):
+    seed = 0
+    for i in list(var):
+        seed += ord(i)
+    random.seed(seed)
+    pic = np.ndarray((int(_h / intensity), int(_w / intensity), 3))
+    for i in range(0, int(_h / intensity)):
+        for j in range(0, int(_w / intensity)):
+            pic[i, j] = (next(rng()))
+    print(pic)
+    pic = Image.fromarray((pic * 255).astype(np.uint8))
+
+    return pic
+
+# Generator for rand_seed function
+def rng():
+    while True:
+        yield np.array([random.random(), random.random(), random.random()])
