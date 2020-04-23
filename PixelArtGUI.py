17659780efcb1d58cbf18acd5ca5d642b3bdf150
btannuzzo <b.y.tannuzzo@gmail.com>
2020-04-22 23:51:14 -0400

    search image can now open file explorer

--- PixelArtGUI.py
+++ PixelArtGUI.py
@@ -16,6 +16,8 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+from browse import *
+
 web = tk.Tk()
 web.configure(background='Black')
 web.geometry("600x600")
@@ -25,10 +27,21 @@ web.title("Pixel Art Generator")
 w = Label(web, text="Welcome to Pixel Art", font=("Ink Free", 36))
 w.pack()
 
+class Browse(Frame):
+    """ Creates a frame that contains a button when clicked lets the user to select
+    a file and put its filepath into an entry.
+    """
 
-class Example(Frame):
+    def __init__(self, master, initialdir='', filetypes=()):
+        super().__init__(master)
+        self.filepath = tk.StringVar()
+        self._initaldir = initialdir
+        self._filetypes = filetypes
+
+class Example(Browse):
     def __init__(self, master, *args):
         Frame.__init__(self, master, *args)
+        Browse.__init__(self, master, initialdir='', filetypes=())
 
         self.image = Image.open("pixels.jpg")
         self.img_copy = self.image.copy()
@@ -101,7 +114,9 @@ class Example(Frame):
             counter += 1
         pic.view_pic(master)
 
+
     # opens a new window to search for image(s)
+    # by Rainie Dormanen
     def search_image(self):
         try:
             if win2.state() == "normal": win2.focus()
@@ -113,7 +128,21 @@ class Example(Frame):
             lb = Label(win2, text="Enter an image or images to get word", fg='red', font=("Ink Free", 25))
             lb.pack()
             frame = Frame(win2)
-            frame.pack(side=BOTTOM)
+            frame.pack(side=TOP)
+            self.entry = Entry(frame, bd=5)
+            self.entry.pack(side=TOP, fill=X)
+            browse_button = Button(frame, text="Browse...", font=("Times New Roman", 10), command=self.browse)
+            browse_button.pack(side=BOTTOM, fill=X, expand = True)
+            file_browser = Browse(web, initialdir=r"C:\Users",
+                                  filetypes=(('jpg files', '*.jpg',),
+                                             ("All files", "*.*")))
+            file_browser.pack(fill='x', expand=True)
+
+    def browse(self):
+        """ Browses a .jpg or .jpeg file or all files and then puts it on the entry.
+        """
+        self.filepath.set(fd.askopenfilename(initialdir=self._initaldir,
+                                                filetypes=self._filetypes))
 
 
 # background image
